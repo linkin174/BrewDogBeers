d846a7631865a4a3a251e2d0df925f30e0a84e4f
Aleksandr Kretov <kretov.1988@gmail.com>
2022-04-13 17:22:18 +0500

    Initial Commit

---
+++ BrewDogBeers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrewDogBeers
+//
+//  Created by Aleksandr Kretov on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
